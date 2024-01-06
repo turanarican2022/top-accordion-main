0f0095f2445e12ede0e9651b113a4c7ba47d7f0e
turanarican2022 <turanarican1985@gmail.com>
2024-01-06 18:47:40 +0300

    project finished

---
+++ script.js
@@ -0,0 +1,16 @@
+const faqHeaders = document.querySelectorAll(".faq-header");
+
+const toggleOpenFAQBody = function () {
+  faqHeaders.forEach((header) => {
+    let thisHeader = header.parentNode;
+    if (thisHeader !== this.parentNode) {
+      thisHeader.firstElementChild.classList.remove("faq-header-active");
+    } else {
+      thisHeader.firstElementChild.classList.toggle("faq-header-active");
+    }
+  });
+};
+
+faqHeaders.forEach((header) => {
+  header.addEventListener("click", toggleOpenFAQBody);
+});
